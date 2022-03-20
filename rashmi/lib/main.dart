6bb39cc26138edc9424b8e263d35b4996b0d274f
Rashmi Vidushani <audrianblackrose@gmail.com>
2022-03-20 09:18:29 -0700

    rr

--- rashmi/lib/main.dart
+++ rashmi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rashmi/commonui/login.dart';
 import 'package:rashmi/splashscreen.dart';
 
 Future<void> main() async {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightGreen,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: const Ilogin(),
     );
   }
 }
